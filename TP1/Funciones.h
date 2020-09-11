6bb0546730b37ca807cbdde43d0ef809a557f1e8
MegodiZX <maximilianoediaz@yahoo.com.ar>
2020-09-11 19:25:31 -0300

    Tp1 test up

---
+++ TP1/Funciones.h
@@ -0,0 +1,20 @@
+int ObtenerEntero(void);
+//Permite ingresar un valor entero para asignarlo en una variable
+
+int Suma(int a, int b);
+//Recibe 2 parametros y los SUMA, devuelve el resultado
+
+int Resta(int a, int b);
+//Recibe 2 parametros y los RESTA, devuelve el resultado
+
+int Producto(int a, int b);
+//Recibe 2 parametros y los MULTIPLICA, devuelve el resultado
+
+float Division(int a, int b);
+//Recibe 2 parametros y los DIVIDE, devuelve el resultado*/
+
+int ObtenerFactorial(int numeroingresado);
+//recibe un numero y con un for lo multiplica por todos los que lo preceden hasta el 1 para sacar el FACTORIAL
+
+void MostarResultados(int suma,int resta,int producto,float division,int factorial1,int factorial2);
+//Recibe los resultados de Suma, Resta, Division, Producto, factorial de A y de B, y los muestra
---
+++ TP1/Menu.h
@@ -0,0 +1,3 @@
+void Calculadora(void);
+//Muestra opciones y se seleccionan al ingresar el numero de la opcion, tambien muestra los valores actuales
+//de A y B para que el usuario sepa que numeros se estan usando
